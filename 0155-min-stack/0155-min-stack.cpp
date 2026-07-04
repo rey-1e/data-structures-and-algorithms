c5ccd8ed4fdeac443e00fec9420bf85356be3fa2
Raj Kanade <kanaderaj1216@gmail.com>
2026-07-05 02:45:21 +0530

    Time: 63 ms (28.14%), Space: 151 MB (58.87%) - LeetHub - Time: O(1), Space: O(n)

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+public:
+
+    stack<pair<int, int>> st; 
+
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(st.empty()) {
+            st.push({value, value});
+        } else {
+            st.push({value, min(st.top().second, value)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
