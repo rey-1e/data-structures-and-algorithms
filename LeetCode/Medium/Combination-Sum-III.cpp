f8d76d42e2ddb23771bb089723049704ff50401c
Raj Kanade <kanaderaj1216@gmail.com>
2026-07-08 19:38:30 +0530

    feat(leetcode): solve Combination Sum III [C++]

--- LeetCode/Medium/Combination-Sum-III.cpp
+++ LeetCode/Medium/Combination-Sum-III.cpp
@@ -1,38 +1,36 @@
 # Combination Sum III
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/combination-sum-iii/submissions/2060573533/
+- URL: https://leetcode.com/problems/combination-sum-iii/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: 7
-ms
-- Memory: 14.67
-MB
-- Solved At: 2026-07-08T14:08:22.234Z
+- Runtime: N/A
+- Memory: N/A
+- Solved At: 2026-07-08T14:08:29.729Z
 
 ## Code
 ```cpp
-class Solution {
+class Solution {
 public:
-    vector<vector<int>> ans;
-
-    void helper(int idx, vector<int>& nums, vector<int> temp, int sum, int k, int n) {
-        //base cases; 
-        if(temp.size() > k || sum > n) return;
-
-        if(sum == n && temp.size() == k) {
-
-            ans.push_back(temp);
-
-            return;
-        }
-
-        if(idx >= nums.size()) return;
-
-
-
-        temp.push_back(nums[idx]);
-        helper(idx + 1, nums, temp, sum + nums[idx], k, n);
-        temp.pop_back();
+    vector<vector<int>> ans;
+    void helper(int idx, vector<int>& nums, vector<int> temp, int sum, int k, int n) {
+        //base cases; 
+        if(temp.size() > k || sum > n) return;
+        if(sum == n && temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+        if(idx >= nums.size()) return;
+        temp.push_back(nums[idx]);
+        helper(idx + 1, nums, temp, sum + nums[idx], k, n);
+        temp.pop_back();
+        
+        //exclusion;
+        helper(idx + 1, nums, temp, sum, k, n);
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //function call and return; 
+        helper(0, nums, {}, 0, k, n);
 ```
