adba9c77a579e167f916d7c68fd618543cdee8be
Raj Kanade <kanaderaj1216@gmail.com>
2026-07-10 15:59:54 +0530

    feat(leetcode): solve Palindrome Linked List [C++]

---
+++ LeetCode/Easy/Palindrome-Linked-List.cpp
@@ -0,0 +1,46 @@
+# Palindrome Linked List
+
+- Platform: LeetCode
+- URL: https://leetcode.com/problems/palindrome-linked-list/submissions/2062745575/
+- Difficulty: Easy
+- Language: C++
+- Status: Accepted
+- Runtime: 0 ms
+- Memory: 118.06
+MB
+- Solved At: 2026-07-10T10:29:53.165Z
+
+## Code
+```cpp
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        
+        ListNode* slow = head;
+        ListNode* fast = head; 
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next; 
+        }
+            fast = fast->next->next; 
+        ListNode* head2 = reverse(slow);
+    ListNode* reverse(ListNode* head) {
+    }
+        ListNode* curr = head; 
+        ListNode* prev = nullptr; 
+        ListNode* next = nullptr; 
+        while(curr != nullptr) {
+            next = curr->next; 
+        }
+            curr->next = prev; 
+            prev = curr; 
+            curr = next; 
+        return prev; 
+        while(head != nullptr && head2 != nullptr) {
+            if(head->val != head2->val) return false;
+        }
+        return true;
+    }
+            head = head->next; 
+            head2 = head2->next; 
+};
+```
