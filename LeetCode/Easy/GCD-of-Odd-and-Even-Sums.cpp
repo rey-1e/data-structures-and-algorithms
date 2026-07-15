f788e6b1cc16738b8fc001ffa655c2cf1597f554
Raj Kanade <kanaderaj1216@gmail.com>
2026-07-15 15:43:43 +0530

    feat(leetcode): solve GCD of Odd and Even Sums [C++]

--- LeetCode/Easy/GCD-of-Odd-and-Even-Sums.cpp
+++ LeetCode/Easy/GCD-of-Odd-and-Even-Sums.cpp
@@ -1,14 +1,14 @@
 # GCD of Odd and Even Sums
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums/submissions/2068460278/?envType=daily-question&envId=2026-07-15
+- URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums/submissions/2068462694/?envType=daily-question&envId=2026-07-15
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 8.42
+- Memory: 8.43
 MB
-- Solved At: 2026-07-15T10:11:58.631Z
+- Solved At: 2026-07-15T10:13:42.476Z
 
 ## Code
 ```cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         
+        return n;
     }
-        return gcd((int)pow(n, 2), n * (n + 1));
 };
 ```
