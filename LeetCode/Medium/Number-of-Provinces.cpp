a41dda432abe15020f242317240958e3853056b8
Raj Kanade <kanaderaj1216@gmail.com>
2026-07-31 15:52:03 +0530

    feat(leetcode): solve Number of Provinces [C++]

--- LeetCode/Medium/Number-of-Provinces.cpp
+++ LeetCode/Medium/Number-of-Provinces.cpp
@@ -1,46 +1,40 @@
 # Number of Provinces
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/number-of-provinces/submissions/2088585297/
+- URL: https://leetcode.com/problems/number-of-provinces/submissions/2088660132/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 20.02
+- Memory: 19.80
 MB
-- Solved At: 2026-07-31T09:14:49.659Z
+- Solved At: 2026-07-31T10:22:03.147Z
 
 ## Code
 ```cpp
-//..
-        // count the number of total traversals;
-        int count = 0;
-        vector<int> visited(isConnected.size(), 0);
-        for (int i = 0; i < visited.size(); i++) {
-            // bfs;
-            queue<int> q;
-            q.push(i);
-            if (visited[i] == 1) {
-                continue;
+int findCircleNum(vector<vector<int>>& isConnected) {
+        
+        vector<int> v(isConnected.size(), 0);
+        int count = 0; 
+        for(int i = 0; i < v.size(); i++) {
+            if(v[i] == 0) {
+                count++;
+                bfs(i, isConnected, v);
+    }
+        while(!q.empty()) {
+            int curr = q.front();
+        }
+            q.pop();
+            for(int i = 0; i < isConnected.size(); i++) {
+                if(isConnected[curr][i] == 1 && !v[i]) {
             }
-            visited[i] = 1;
-            while (!q.empty()) {
-                int curr = q.front();
-                q.pop();
-                for (int j = 0; j < isConnected.size(); j++) {
-                    if (isConnected[curr][j] == 1 && !visited[j]) {
+                    q.push(i);
                 }
-                        visited[j] = 1;
-                    }
-            count++;
-        // traversal from the next unvisited node to the entire comopnent.
-        //..
-        //..
-        // traversal from first unvisited node to the entire component.
+                    v[i] = 1; 
+        return; 
+        q.push(start);
+        v[start] = 1; 
+        queue<int> q; 
             }
         }
-        return count;
-                        q.push(j);
-    }
-};
 ```
