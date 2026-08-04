73aab507b0b42331f43f3838adec4aae1ec2b1ed
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-05 02:40:56 +0530

    feat(leetcode): solve Rotting Oranges [C++]

--- LeetCode/Medium/Rotting-Oranges.cpp
+++ LeetCode/Medium/Rotting-Oranges.cpp
@@ -1,39 +1,16 @@
 # Rotting Oranges
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/rotting-oranges/submissions/2093996978/
+- URL: https://leetcode.com/problems/rotting-oranges/description/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: 0 ms
-- Memory: 0.00
-MB
-- Solved At: 2026-08-04T13:02:03.382Z
+- Runtime: N/A
+- Memory: N/A
+- Solved At: 2026-08-04T21:10:55.910Z
 
 ## Code
 ```cpp
-if (changeOccurred) {
-                count++;
-            } else
-                    if (grid[CR][CC] == 1) {
-                        q.push({CR, CC});
-                        grid[CR][CC] = 2;
-                    }
-                }
-            }
-                    if (CR < 0 || CC < 0 || CR >= grid.size() ||
-                        CC >= grid[0].size()) {
-                        continue;
-                    } else if (grid[CR][CC] == 1) {
-                        changeOccurred = true;
-                    }
-                for (int i = 0; i < 4; i++) {
-                    int CR = curr.first + rows[i];
-                    int CC = curr.second + cols[i];
-            bool changeOccurred = false;
-            for (int temp = 0; temp < currSize; temp++) {
-                auto curr = q.front();
-                q.pop();
 class Solution {
 public:
     int bfs(vector<vector<int>>& grid, queue<pair<int, int>>& q) {
@@ -42,4 +19,26 @@ public:
             int currSize = q.size();
             int rows[4] = {0, -1, 0, +1};
             int cols[4] = {-1, 0, +1, 0};
+            bool changeOccurred = false;
+            for (int temp = 0; temp < currSize; temp++) {
+                auto curr = q.front();
+                q.pop();
+                for (int i = 0; i < 4; i++) {
+                    int CR = curr.first + rows[i];
+                    int CC = curr.second + cols[i];
+                    if (CR < 0 || CC < 0 || CR >= grid.size() ||
+                        CC >= grid[0].size()) {
+                        continue;
+                    } else if (grid[CR][CC] == 1) {
+                        changeOccurred = true;
+                    }
+                    if (grid[CR][CC] == 1) {
+                        q.push({CR, CC});
+                        grid[CR][CC] = 2;
+                    }
+                }
+            }
+            if (changeOccurred) {
+                count++;
+            } else
 ```
