0c1e902f92a2b5e7581f2b3589e43a1d420f46b1
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-08 14:18:46 +0530

    feat(gfg): solve Distance of nearest cell having 1 | Practice | GeeksforGeeks [C++]

---
+++ GeeksForGeeks/Medium/Distance-of-nearest-cell-having-1---Practice---GeeksforGeeks.cpp
@@ -0,0 +1,62 @@
+# Distance of nearest cell having 1 | Practice | GeeksforGeeks
+
+- Platform: GeeksForGeeks
+- URL: https://www.geeksforgeeks.org/problems/distance-of-nearest-cell-having-1-1587115620/1
+- Difficulty: Medium
+- Language: C++
+- Status: Accepted
+- Runtime: N/A
+- Memory: N/A
+- Solved At: 2026-08-08T08:48:45.576Z
+
+## Code
+```cpp
+for(int i = 0; i < 4; i++ ) {
+                //convert the nearest -1's to the distance of itself{count};
+                int row = curr.first + delRow[i];
+                int col = curr.second + delCol[i];
+                
+                //break condition; 
+                if(row < 0 || row >= distance.size() || col < 0 || col >= distance[0].size()) continue; 
+                
+                if(distance[row][col] == -1) {
+                    distance[row][col] = count; 
+                    q.push({row, col});
+                }
+            }   
+            }
+        }
+    }
+    
+  
+    vector<vector<int>> nearest(vector<vector<int>>& grid) {
+        // code here
+        if(grid.size() == 0) return {};
+        
+        int n = grid.size(); 
+        int m = grid[0].size();
+        
+        vector<vector<int>> distance(n, vector<int>(m, -1));
+        
+        queue<pair<int, int>> q; 
+        bool hasOne = false;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    hasOne = true;
+                    distance[i][j] = 0; 
+                    q.push({i, j});
+                }
+            }
+        }
+        
+        if(!hasOne) return {};
+        
+        //call for bfs searcher. 
+        construct(q, distance);
+        
+        
+        return distance;
+    }
+```
