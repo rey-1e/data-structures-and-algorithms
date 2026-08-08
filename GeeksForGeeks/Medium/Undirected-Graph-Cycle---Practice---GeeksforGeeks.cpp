2d2422640f3038e9f22fac9f7121a96a1a560dec
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-08 13:02:45 +0530

    feat(gfg): solve Undirected Graph Cycle | Practice | GeeksforGeeks [C++]

--- GeeksForGeeks/Medium/Undirected-Graph-Cycle---Practice---GeeksforGeeks.cpp
+++ GeeksForGeeks/Medium/Undirected-Graph-Cycle---Practice---GeeksforGeeks.cpp
@@ -7,57 +7,51 @@
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-08-07T10:48:43.221Z
+- Solved At: 2026-08-08T07:32:44.805Z
 
 ## Code
 ```cpp
 class Solution {
   public:
-  
-    bool detect(int src, vector<vector<int>>& adj, vector<int>& visited) {
-        //push into the queue and mark it visited; 
-        visited[src] = 1; 
-        queue<pair<int, int>> q; 
-        q.push({src, -1});
+    bool dfs(int parent, int src, vector<vector<int>>& adj, vector<int>& visited) {
         
-        while(!q.empty()) {
-            int node = q.front().first;
-            int parent = q.front().second;
-            
-            q.pop();
-            
-            for(auto val : adj[node]) {
-                if(visited[val] == 0) {
-                    q.push({val, node});
-                    visited[val] = 1; 
-                } else if(visited[val] == 1 && val != parent) {
-                    return true; 
-                }
+        visited[src] = 1; 
+        
+        for(auto val : adj[src]) {
+            if(visited[val] == 0) {
+                //simply call;
+                if (dfs(src, val, adj, visited)) return true;
+            } else if(val != parent) {
+                return true; 
             }
-            
         }
         
         
         return false;
     }
   
+  
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
+        vector<vector<int>> adj(V);
         
-        
-        //create an adjacency list; 
-        vector<vector<int>> adj(V); 
         for(auto edge : edges) {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
         
-        //create a visited array; 
+        
         vector<int> visited(V, 0);
         
         for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                //call dfs; 
-                if(detect(i, adj, visited)) {
-                    return true;
+            if(visited[i] == 0) {
+                if(dfs(-1, i, adj, visited)) return true; 
+            }
+        }
+        
+        
+        
+        return false;
+    }
+};
 ```
