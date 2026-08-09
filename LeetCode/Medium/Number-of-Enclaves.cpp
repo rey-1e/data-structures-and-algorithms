c538834407a71592637a7dcc844154e559c9fa5b
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-09 15:17:30 +0530

    feat(leetcode): solve Number of Enclaves [C++]

--- LeetCode/Medium/Number-of-Enclaves.cpp
+++ LeetCode/Medium/Number-of-Enclaves.cpp
@@ -1,14 +1,14 @@
 # Number of Enclaves
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/number-of-enclaves/submissions/2100177272/
+- URL: https://leetcode.com/problems/number-of-enclaves/submissions/2100178105/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 36.16
+- Memory: 33.24
 MB
-- Solved At: 2026-08-09T09:46:03.002Z
+- Solved At: 2026-08-09T09:47:29.786Z
 
 ## Code
 ```cpp
@@ -16,15 +16,18 @@ int numEnclaves(vector<vector<int>>& grid) {
         
         int n= grid.size(); 
         int m = grid[0].size();
-        vector<vector<int>> matrix = grid;
  
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
                     //call bfs if it's a one & convert them to '#';
     }
-                    if(matrix[i][j] == 1) {
-                        dfs(i, j, matrix);
+                    if(grid[i][j] == 1) {
+                        dfs(i, j, grid);
+        return; 
+        }
+            }
+            dfs(R, C, matrix);
                     }
                 }
             }
@@ -32,12 +35,10 @@ int numEnclaves(vector<vector<int>>& grid) {
         int count = 0; 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(matrix[i][j] == 1) {
+                if(grid[i][j] == 1) {
                     count++;
                 }
             }
         }
         return count;
-    }
-};
 ```
