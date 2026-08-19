8951fc1d7832d252077bb51b26270a00fd1b6639
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-19 21:37:55 +0530

    feat(gfg): solve Directed Graph Cycle | Practice | GeeksforGeeks [C++]

--- GeeksForGeeks/Medium/Directed-Graph-Cycle---Practice---GeeksforGeeks.cpp
+++ GeeksForGeeks/Medium/Directed-Graph-Cycle---Practice---GeeksforGeeks.cpp
@@ -7,27 +7,25 @@
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-08-13T13:12:57.336Z
+- Solved At: 2026-08-19T16:07:54.159Z
 
 ## Code
 ```cpp
-pathVisited[node] = 1; 
-        
-        
-        //call the dfs functiono again; 
-        for(auto& val : edges[node]) {
-            if(visited[val] == 0) {
-                //node is not visited; 
-                if(dfs(val, edges, visited, pathVisited)) return true;
-            } else if(pathVisited[val] == 1){
-                //we will see; 
-                return true; 
+//call for the entire adjacent nodes; 
+        for(auto val : adj[node]) {
+            if(!visited[val]) {
                 
+                if(hasCycle(val, adj, visited, pathVisited)) return true; 
+                
+            } else if(pathVisited[val]) {
+                return true; 
             }
+            
+            
         }
         
         
-        //back track; 
+        
         pathVisited[node] = 0; 
         return false;
         
@@ -35,26 +33,29 @@ pathVisited[node] = 1;
   
     bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
-        vector<int> visited(V);
-        vector<int> pathVisited(V);
+        vector<vector<int>> adj(V); 
         
-        //create an adjacency matrix; 
-        vector<vector<int>> adj(V);
+        //adjancency list; 
         for(auto edge : edges) {
             adj[edge[0]].push_back(edge[1]);
         }
         
+        //visited array; 
+        vector<int> visited(V, 0); 
+        //pathVisited array; 
+        vector<int> pathVisited(V, 0); 
         
-        for(int i  =0; i < V; i++) {
-            if(visited[i] == 0) {
-                //this is unvisited
-                if(dfs(i, adj, visited, pathVisited)) return true; 
+        for(int i = 0; i < visited.size(); i++) {
+            if(!visited[i]) {
+                //call the dfs algo;
+                if(hasCycle(i, adj, visited, pathVisited)) return true; 
                 
             }
         }
         
         
         return false;
+        
         
     }
 };
