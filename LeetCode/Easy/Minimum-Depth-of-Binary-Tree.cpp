d2cd94707b61967f033045514d4ddc8252b3c90c
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-19 22:58:31 +0530

    feat(leetcode): solve Minimum Depth of Binary Tree [C++]

--- LeetCode/Easy/Minimum-Depth-of-Binary-Tree.cpp
+++ LeetCode/Easy/Minimum-Depth-of-Binary-Tree.cpp
@@ -1,40 +1,47 @@
 # Minimum Depth of Binary Tree
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/minimum-depth-of-binary-tree/submissions/2113025207/?envType=problem-list-v2&envId=breadth-first-search
+- URL: https://leetcode.com/problems/minimum-depth-of-binary-tree/submissions/2113031776/?envType=problem-list-v2&envId=breadth-first-search
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 146.63
+- Memory: 146.94
 MB
-- Solved At: 2026-08-19T17:24:31.899Z
+- Solved At: 2026-08-19T17:28:30.499Z
 
 ## Code
 ```cpp
 public:
-    int helper(TreeNode* root) {
-        if(root == nullptr) {
-            return 0; 
-        }
-        int leftHeight = helper(root->left);
-        int rightHeight = helper(root->right);
-        int val;
-        if(leftHeight && rightHeight) {
-            val = min(leftHeight, rightHeight) + 1; 
-        } else if(leftHeight) {
-            val = leftHeight + 1; 
-        } else val = rightHeight + 1; 
-        
-        return val; 
-    } 
     int minDepth(TreeNode* root) {
-        
-        return helper(root);
-    }
-};
+            //using the minimum depth approach; 
+class Solution {
+        queue<TreeNode*> q; 
+        q.push(root);
+        int count = 0; 
+        while(!q.empty()) {
+            count++;
+            int currSize = q.size();
+                q.pop();
+            for(int i = 0; i < currSize; i++) {
+                TreeNode* curr = q.front();
+                if(curr->left) {
+                if(curr->left == nullptr && curr->right == nullptr) {
+                    return count; 
+                }
  (right) {}
  * };
  */
-class Solution {
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+/**
+        if(root == nullptr) return 0; 
+            
+                    q.push(curr->left);
 ```
