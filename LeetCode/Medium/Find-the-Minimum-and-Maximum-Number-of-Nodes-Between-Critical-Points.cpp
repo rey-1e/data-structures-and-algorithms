ab6e5e685302456885c3542998b3fb75bd3e8b39
Raj Kanade <kanaderaj1216@gmail.com>
2026-08-31 16:27:58 +0530

    feat(leetcode): solve Find the Minimum and Maximum Number of Nodes Between Critical Points [C++]

--- LeetCode/Medium/Find-the-Minimum-and-Maximum-Number-of-Nodes-Between-Critical-Points.cpp
+++ LeetCode/Medium/Find-the-Minimum-and-Maximum-Number-of-Nodes-Between-Critical-Points.cpp
@@ -1,43 +1,37 @@
 # Find the Minimum and Maximum Number of Nodes Between Critical Points
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/submissions/2126002773/?envType=daily-question&envId=2026-08-31
+- URL: https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/submissions/2126008404/?envType=daily-question&envId=2026-08-31
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 128.48
+- Memory: 124.65
 MB
-- Solved At: 2026-08-31T10:51:38.486Z
+- Solved At: 2026-08-31T10:57:57.829Z
 
 ## Code
 ```cpp
-vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-            
-        if(head == nullptr || head->next == nullptr) return {-1, -1};
-        
-        vector<int> criticalPoints;
-        ListNode* prev = head; 
-        ListNode* temp = head->next; 
-        int i = 1; 
-        while(temp != nullptr && temp->next != nullptr) {
-            //maximum;
-            if(prev->val < temp->val && temp->next->val < temp->val) {
-                criticalPoints.push_back(i);
+prevCritical = i;
             }
             //minimum; 
             if(prev->val > temp->val && temp->next->val > temp->val) {
-                criticalPoints.push_back(i);
+                if(firstCritical == -1) {
+                    firstCritical = i; 
+                }
+                if(prevCritical != -1) {
+                minDistance = min(minDistance, i - prevCritical);
+                }
+                prevCritical = i; 
             }
             i++;
+            prev = temp; 
             temp = temp->next; 
         }
-            prev = temp; 
-        int maxDistance = INT_MIN; 
-        int minDistance = INT_MAX;
-        if(criticalPoints.size() <= 1) {
+        if(minDistance == INT_MAX) {
             return {-1, -1};
         }
-        
-        maxDistance = criticalPoints.back() - criticalPoints[0];
+        return {minDistance, prevCritical - firstCritical};
+    }
+};
 ```
