1a394198ff2c1d09316f3941b1b21d0db8c237ef
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-02 16:42:57 +0530

    feat(leetcode): solve Construct Uniform Parity Array I [C++]

---
+++ LeetCode/Easy/Construct-Uniform-Parity-Array-I.cpp
@@ -0,0 +1,21 @@
+# Construct Uniform Parity Array I
+
+- Platform: LeetCode
+- URL: https://leetcode.com/problems/construct-uniform-parity-array-i/submissions/2128354176/?envType=daily-question&envId=2026-09-02
+- Difficulty: Easy
+- Language: C++
+- Status: Accepted
+- Runtime: 0 ms
+- Memory: 30.20
+MB
+- Solved At: 2026-09-02T11:12:56.165Z
+
+## Code
+```cpp
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+```
