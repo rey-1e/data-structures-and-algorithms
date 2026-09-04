747d97ee3c248e503b7a1f1425161374723bf631
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-05 02:19:44 +0530

    feat(leetcode): solve Group Anagrams [C++]

--- LeetCode/Medium/Group-Anagrams.cpp
+++ LeetCode/Medium/Group-Anagrams.cpp
@@ -1,23 +1,18 @@
 # Group Anagrams
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/group-anagrams/submissions/2131130872/
+- URL: https://leetcode.com/problems/group-anagrams/submissions/2131131828/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 32.19
+- Memory: 31.84
 MB
-- Solved At: 2026-09-04T20:47:41.984Z
+- Solved At: 2026-09-04T20:49:43.872Z
 
 ## Code
 ```cpp
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) 
-    {
-        
-        map<vector<int>, vector<int>> m; 
+map<vector<int>, vector<string>> m; 
         for(int i = 0; i < strs.size(); i++) {
             //for each character calculate the vector; 
         }
@@ -25,17 +20,17 @@ public:
             for(auto& c : strs[i]) {
                 V[c - 'a']++;
             }
-            m[V].push_back(i);
+            m[V].push_back(strs[i]);
         //the words that yield the same vector are in 
         //one map key; 
         for(auto& [key, vec] : m) {
-            vector<string> store; 
         }
-                store.push_back(strs[val]);
-            }
-            for(auto& val : vec) {
-            ans.push_back(store);
+            ans.push_back(vec);
         return ans;
         vector<vector<string>> ans; 
     }
+};
+    {
+        
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
 ```
