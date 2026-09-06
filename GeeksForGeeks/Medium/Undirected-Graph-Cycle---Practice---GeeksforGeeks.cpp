d04fab0f8f76d94e73e160f4a0e539d887b245f1
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 01:03:27 +0530

    feat(gfg): solve Undirected Graph Cycle | Practice | GeeksforGeeks [C++]

--- GeeksForGeeks/Medium/Undirected-Graph-Cycle---Practice---GeeksforGeeks.cpp
+++ GeeksForGeeks/Medium/Undirected-Graph-Cycle---Practice---GeeksforGeeks.cpp
@@ -7,51 +7,53 @@
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-08-08T07:32:44.805Z
+- Solved At: 2026-09-06T19:33:26.864Z
 
 ## Code
 ```cpp
 class Solution {
   public:
-    bool dfs(int parent, int src, vector<vector<int>>& adj, vector<int>& visited) {
+    bool bfs(int node, vector<vector<int>>& adj, vector<int>& visited) {
         
-        visited[src] = 1; 
-        
-        for(auto val : adj[src]) {
-            if(visited[val] == 0) {
-                //simply call;
-                if (dfs(src, val, adj, visited)) return true;
-            } else if(val != parent) {
-                return true; 
+        queue<pair<int, int>> q; 
+        visited[node] = 1; 
+        q.push({node, -1});
+        //CHECK OR KEEP TRACK OF THE PARENT NODE THIS IS ABSOLUTELY IMPORTANT FOR UNDIRECTED
+        //GRAPHS CYCLES...............
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            for(auto& val : adj[curr.first]) {
+                if(visited[val] == 0) {
+                    visited[val] = 1; 
+                    q.push({val, curr.first});
+                } else if(val != curr.second) {
+                    return true; 
+                }
             }
         }
         
-        
         return false;
     }
-  
   
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
         vector<vector<int>> adj(V);
         
-        for(auto edge : edges) {
+        for(auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
             adj[edge[1]].push_back(edge[0]);
         }
         
-        
         vector<int> visited(V, 0);
         
-        for(int i = 0; i < V; i++) {
+        for(int i = 0; i < visited.size(); i++) {
             if(visited[i] == 0) {
-                if(dfs(-1, i, adj, visited)) return true; 
+                if(bfs(i, adj, visited)) {
+                    return true; 
+                }
             }
         }
         
-        
-        
         return false;
-    }
-};
 ```
