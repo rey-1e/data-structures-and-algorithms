540a70e5293caae542fe7a1ed21401a0d20f5e5c
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 00:47:01 +0530

    feat(leetcode): solve Rotting Oranges [C++]

--- LeetCode/Medium/Rotting-Oranges.cpp
+++ LeetCode/Medium/Rotting-Oranges.cpp
@@ -1,44 +1,48 @@
 # Rotting Oranges
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/rotting-oranges/description/
+- URL: https://leetcode.com/problems/rotting-oranges/submissions/2133232489/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: N/A
-- Memory: N/A
-- Solved At: 2026-08-04T21:10:55.910Z
+- Runtime: 0 ms
+- Memory: 16.76
+MB
+- Solved At: 2026-09-06T19:17:00.371Z
 
 ## Code
 ```cpp
-class Solution {
-public:
-    int bfs(vector<vector<int>>& grid, queue<pair<int, int>>& q) {
-        int count = 0;
-        while (!q.empty()) {
-            int currSize = q.size();
-            int rows[4] = {0, -1, 0, +1};
-            int cols[4] = {-1, 0, +1, 0};
-            bool changeOccurred = false;
-            for (int temp = 0; temp < currSize; temp++) {
-                auto curr = q.front();
+while(!q.empty()) {
+            int sz = q.size();
+            bool hasRotten = false;
+        int delRow[] = {0, -1, 0, +1};
+        int delCol[] = {-1, 0, +1, 0};
+        }   
+        int time = 0;
+                    if(R < n && C < m && R >= 0 && C >= 0 && 
+                        grid[R][C] == 1) {
+                    int C = curr.second + delCol[i];
+                    int R = curr.first + delRow[i]; 
+                for(int i = 0; i < 4; i++) {
                 q.pop();
-                for (int i = 0; i < 4; i++) {
-                    int CR = curr.first + rows[i];
-                    int CC = curr.second + cols[i];
-                    if (CR < 0 || CC < 0 || CR >= grid.size() ||
-                        CC >= grid[0].size()) {
-                        continue;
-                    } else if (grid[CR][CC] == 1) {
-                        changeOccurred = true;
-                    }
-                    if (grid[CR][CC] == 1) {
-                        q.push({CR, CC});
-                        grid[CR][CC] = 2;
-                    }
-                }
+                auto curr = q.front();
+                //traverse the surrounding and push into the queue. 
+            for(int i = 0; i < sz; i++) {
+                }                
             }
-            if (changeOccurred) {
-                count++;
-            } else
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+        queue<pair<int, int>> q; 
+        for(int i = 0; i < n; i++) {
+                            grid[R][C] = 2; 
+                            q.push({R, C});
+                        }
+                }
+                
+            }
+            if(hasRotten) {
+                            hasRotten = true; 
+                time++;
+            }
 ```
