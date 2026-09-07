28fb272bb7656269212ecd8bd194838cc22fa8f6
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 18:50:43 +0530

    feat(leetcode): solve Number of Islands [C++]

--- LeetCode/Medium/Number-of-Islands.cpp
+++ LeetCode/Medium/Number-of-Islands.cpp
@@ -1,45 +1,50 @@
 # Number of Islands
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/number-of-islands/description/
+- URL: https://leetcode.com/problems/number-of-islands/submissions/2133915286/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: N/A
-- Memory: N/A
-- Solved At: 2026-08-03T21:07:06.786Z
+- Runtime: 0 ms
+- Memory: 24.31
+MB
+- Solved At: 2026-09-07T13:20:43.159Z
 
 ## Code
 ```cpp
-class Solution {
-public:
-    void traverse(int i, int j, vector<vector<char>>& grid) {
-        // left, right, up, down.
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> q;
-        q.push({i, j});
-        grid[i][j] = '#';
-        while (!q.empty()) {
-            auto curr = q.front();
-            q.pop();
-            j = curr.second;
-            i = curr.first;
-            if (j + 1 < n && grid[i][j + 1] == '1') {
-                grid[i][j + 1] = '#';
-                q.push({i, j + 1});
-            }
-            if (i + 1 < m && grid[i + 1][j] == '1') {
-                grid[i + 1][j] = '#';
-                q.push({i + 1, j});
-            }
-            if (j - 1 >= 0 && grid[i][j - 1] == '1') {
-                grid[i][j - 1] = '#';
-                q.push({i, j - 1});
-            }
-            if (i - 1 >= 0 && grid[i - 1][j] == '1') {
-                grid[i - 1][j] = '#';
-                q.push({i - 1, j});
-            }
+int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0; 
+        vector<vector<int>> visited(n, vector<int>(m, 0)); 
+        for(int i= 0; i < n ;i++) {
+            for(int j = 0; j < m; j++) {
+                if(visited[i][j] == 0 && grid[i][j] == '1') {
+    }
         }
+            q.pop();
+            for(int i= 0; i < 4; i++) {
+                int R = row + delRow[i]; 
+            }
+                int C = col + delCol[i]; 
+                if(R >= 0 && C >= 0 && R < grid.size() && C < grid[0].size()
+                   && visited[R][C] == 0 && grid[R][C] == '1') {
+                    //this is a part of an island;
+                }
+                    visited[R][C] = 1; 
+                    q.push({R, C});
+        return; 
+            int col = curr.second;
+            int row = curr.first;
+            auto curr = q.front();
+        while(!q.empty()) {
+        int delCol[] = {-1, 0, +1, 0};
+        int delRow[] = {0, -1, 0, +1};
+        visited[i][j] = 1; 
+        q.push({i, j});
+        queue<pair<int, int>> q; 
+public:
+    void bfs(int i, int j, vector<vector<char>>& grid, vector<vector<int>>& visited) {
+        
+class Solution {
 ```
