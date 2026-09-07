e0626c04c924909fd9cf45857b22825820bb54e5
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 16:44:51 +0530

    feat(leetcode): solve 01 Matrix [C++]

--- LeetCode/Medium/01-Matrix.cpp
+++ LeetCode/Medium/01-Matrix.cpp
@@ -1,42 +1,47 @@
 # 01 Matrix
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/01-matrix/submissions/2123181555/?envType=problem-list-v2&envId=breadth-first-search
+- URL: https://leetcode.com/problems/01-matrix/submissions/2133812210/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 37.46
+- Memory: 37.34
 MB
-- Solved At: 2026-08-28T18:09:36.060Z
+- Solved At: 2026-09-07T11:14:50.605Z
 
 ## Code
 ```cpp
-}
-    
-        int row[] = {0, -1, 0, +1};
-        int col[] = {-1, 0, +1, 0};
-            int val = 1; //adjacent values; 
-            int levelSize = q.size();
-            for(int i = 0; i < levelSize; i++) {
+int dist = 0; 
+        while(!q.empty()) {
+            int sz = q.size();
+            dist++;
+            for(int i = 0; i < sz; i++) {
                 auto curr = q.front();
                 q.pop();
-                for(int j = 0; j < 4; j++) {
+                int row = curr.first;
+                int col = curr.second;
+        
+        int delRow[] = {0, -1, 0, +1};
+        int delCol[] = {-1, 0, +1, 0};
+                for(int i = 0; i < 4; i++) {
+                    int R = row + delRow[i];
+                    int C = col + delCol[i]; 
+                    if(R >= 0 && C >= 0 && R < n && C < m && 
+                       visited[R][C] == 0) {
+                        visited[R][C] = 1; 
+                        ans[R][C] = dist;
+                        q.push({R, C});
+                       }
                 }
             }
-                    visited[i][j] = 1; 
-                    q.push({i, j});
-            for(int j = 0; j < m; j++) {
+                }
+            }
+        }
                 if(mat[i][j] == 0) {
-        while(!q.empty()) {
-        queue<pair<int, int>> q; 
-        for(int i = 0; i < n; i++ ){
-        vector<vector<int>> grid(n, vector<int>(m, 0));
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        
-        int n = mat.size(); 
-        int m = mat[0].size();
-public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-class Solution {
+                    q.push({i, j});
+                    visited[i][j] = 1; 
+        for(int i= 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+        queue<pair<int, int>> q;
 ```
