6a46395529c51c67530df8f97f97d15ca7b63c49
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 21:35:42 +0530

    feat(gfg): solve Topological Sort | Practice | GeeksforGeeks [C++]

--- GeeksForGeeks/Medium/Topological-Sort---Practice---GeeksforGeeks.cpp
+++ GeeksForGeeks/Medium/Topological-Sort---Practice---GeeksforGeeks.cpp
@@ -7,7 +7,7 @@
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-08-20T18:22:57.228Z
+- Solved At: 2026-09-07T16:05:41.212Z
 
 ## Code
 ```cpp
@@ -16,41 +16,41 @@ class Solution {
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
         
-        //create an adjacency list; 
+        //indegree vector; 
+        vector<int> indegree(V, 0);
+        //create an ajdacency list; 
         vector<vector<int>> adj(V);
         
-        vector<int> indegree(V); 
-        
-        for(auto edge : edges) {
+        for(auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
-            //include the degree part as well; 
-            indegree[edge[1]]++; 
+            indegree[edge[1]]++;
         }
         
-        //answer vector;         
-        vector<int> arr; 
         queue<int> q; 
-        
         for(int i = 0; i < V; i++) {
-            if(indegree[i] == 0) q.push(i);
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
         }
-        
+        vector<int> vec; 
         while(!q.empty()) {
-            int curr = q.front();
+            auto curr = q.front(); //current node; 
             q.pop();
-            
-            arr.push_back(curr);
+            vec.push_back(curr);
             
             for(auto& val : adj[curr]) {
                 indegree[val]--;
-                
                 if(indegree[val] == 0) {
                     q.push(val);
                 }
             }
         }
         
+        if(vec.size() == V) return vec; 
         
-        return arr; 
+        return {};
+        
+        
     }
+};
 ```
