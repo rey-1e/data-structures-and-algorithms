d94ac3445781ef439104d224c2aaaf47b744fb8d
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 17:17:59 +0530

    feat(leetcode): solve Number of Enclaves [C++]

--- LeetCode/Medium/Number-of-Enclaves.cpp
+++ LeetCode/Medium/Number-of-Enclaves.cpp
@@ -1,46 +1,47 @@
 # Number of Enclaves
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/number-of-enclaves/submissions/2100238897/
+- URL: https://leetcode.com/problems/number-of-enclaves/submissions/2133836860/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 28.83
+- Memory: 36.06
 MB
-- Solved At: 2026-08-09T10:51:05.333Z
+- Solved At: 2026-09-07T11:47:58.967Z
 
 ## Code
 ```cpp
-int delCol[] = {-1, 0, +1, 0};
-        while(!q.empty()) {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++) {
-                int R = row + delRow[i];
-                int C = col + delCol[i];
-                if(R >= 0 && C >= 0 && R < n && C < m && grid[R][C] == 1) {
-                    q.push({R, C});
-        int delRow[] = {0, -1, 0, +1};
-                    grid[R][C] = '#';
-        }
-            }
-                    }
-                }
-                        grid[i][j] = '#';
-                        q.push({i, j});
-                    if(grid[i][j] == 1) {
-                    //call bfs if it's a one & convert them to '#';
-                if(i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-        int n= grid.size(); 
+int numEnclaves(vector<vector<int>>& grid) {
+        
+        int n = grid.size(); 
+        vector<vector<int>> visited(n, vector<int>(m, 0));
         int m = grid[0].size();
-        queue<pair<int, int>> q; 
- 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-    int numEnclaves(vector<vector<int>>& grid) {
-        
-class Solution {
+    }
+        }
+            for(int i = 0; i < 4; i++) {
+                int R = row + delRow[i]; 
+            }
+                int C = col + delCol[i];
+                if(R >= 0 && C >= 0 && R < grid.size() && C < grid[0].size() 
+                   && grid[R][C] == 1 && visited[R][C] == 0) {
+                    visited[R][C] = 1; 
+                   }
+                    q.push({R, C});
+        return; 
+            int col = curr.second;
+            auto curr = q.front();
+            int row = curr.first;
+        while(!q.empty()) {
+        int delRow[] = {0, -1, 0, +1};
+        int delCol[] = {-1, 0, +1, 0};
+        queue<pair<int, int>> q; 
+        q.push({i, j});
+        visited[i][j] = 1; 
+    void bfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& visited) {
 public:
+class Solution {
+            q.pop();
 ```
