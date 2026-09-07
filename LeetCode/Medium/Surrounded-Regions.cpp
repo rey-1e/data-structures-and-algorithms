0618d49cae079a462c8b298ac73489e5579d2c8b
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-07 17:09:08 +0530

    feat(leetcode): solve Surrounded Regions [C++]

--- LeetCode/Medium/Surrounded-Regions.cpp
+++ LeetCode/Medium/Surrounded-Regions.cpp
@@ -1,46 +1,44 @@
 # Surrounded Regions
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/surrounded-regions/submissions/2113806305/?envType=problem-list-v2&envId=breadth-first-search
+- URL: https://leetcode.com/problems/surrounded-regions/submissions/2133830432/
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
 - Runtime: 0 ms
-- Memory: 14.56
+- Memory: 14.44
 MB
-- Solved At: 2026-08-20T10:25:35.220Z
+- Solved At: 2026-09-07T11:39:07.904Z
 
 ## Code
 ```cpp
-}
-                // right edge
-                if (j == m - 1 && board[i][j] == 'O') {
-                    bfs( i,  j, board);
-                }
-                // top edge
-                if (i == 0 && board[i][j] == 'O') {
-                    bfs( i,  j, board);
-                }
-                // bottom edge
-                if (i == n - 1 && board[i][j] == 'O') {
-                    bfs( i,  j, board);
-                }
-            }
+void solve(vector<vector<char>>& board) {
+        
+        int n = board.size(); 
+        int m = board[0].size();
+        return; 
+    }
         }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(board[i][j] == '#') {
-                    board[i][j] = 'O';
-                } else if(board[i][j] == 'O') {
-                    board[i][j] = 'X';
-                }
+                   }
             }
-        }
-                    // dfs or bfs whatever;
-                    // bfs for converting all to '#';
-                    bfs( i,  j, board);
-                // left edge
-                if (j == 0 && board[i][j] == 'O') {
-                    // do it here;
-            for (int j = 0; j < m; j++) {
+                    q.push({R, C});
+                   && board[R][C] == 'O') {
+                    board[R][C] = '#'; //mark visited; 
+                int C = col + delCol[i]; 
+                if(R >= 0 && C >= 0 && R < board.size() && C < board[0].size()
+            q.pop();
+            for(int i = 0; i < 4; i++) {
+                int R = row + delRow[i]; 
+            int row = node.first;
+            int col = node.second;
+        while(!q.empty()) {
+            auto node = q.front();
+        int delRow[] = {0, -1, 0, +1};
+        int delCol[] = {-1, 0, +1, 0};
+        queue<pair<int, int>> q; 
+        q.push({i, j});
+        board[i][j] = '#';
+class Solution {
+public:
+    void convert(int i, int j, vector<vector<char>>& board) {
 ```
