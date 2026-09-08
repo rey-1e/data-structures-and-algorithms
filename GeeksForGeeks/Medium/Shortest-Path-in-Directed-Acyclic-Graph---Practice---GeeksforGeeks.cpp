bcf19ecda4ca40e41446b77d4b620f5d0082c845
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-08 12:54:40 +0530

    feat(gfg): solve Shortest Path in Directed Acyclic Graph | Practice | GeeksforGeeks [C++]

--- GeeksForGeeks/Medium/Shortest-Path-in-Directed-Acyclic-Graph---Practice---GeeksforGeeks.cpp
+++ GeeksForGeeks/Medium/Shortest-Path-in-Directed-Acyclic-Graph---Practice---GeeksforGeeks.cpp
@@ -7,49 +7,51 @@
 - Status: Accepted
 - Runtime: N/A
 - Memory: N/A
-- Solved At: 2026-08-29T08:44:48.749Z
+- Solved At: 2026-09-08T07:24:39.909Z
 
 ## Code
 ```cpp
-}
+vector<int> shortestPath(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int, int>>> adj(V);
         
+        //adjacency vector; 
+        for(auto& edge : edges) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+        }
         
-        //we now have toposort in our stack; 
-        
-        //distance array; 
         vector<int> dist(V, INT_MAX);
+        dist[0] = 0; 
         
-        //hardcode src as 0; 
-        int src = 0; 
+        queue<pair<int, int>> q; 
+        q.push({0, 0});
         
-        dist[src] = 0; 
-        
-        while(!st.empty()) {
-            //curr node; 
-            int curr = st.top();
-            st.pop();
+        while(!q.empty()) {
+            auto curr = q.front();
+            int distance = curr.first;
+            int node = curr.second;
             
-            for(auto edge : adj[curr]) {
-                int v = edge.first;
-                int wt = edge.second;
+            q.pop();
+            
+            for(auto& val : adj[node]) {
+                int adjNode = val.first;    
+                int adjWeight = val.second;
                 
-                if(dist[curr] != INT_MAX) {
-                
-                    if(dist[curr] + wt < dist[v]) {
-                        dist[v] = dist[curr] + wt; 
-                    }
-                    
+                if(adjWeight + distance < dist[adjNode]) {
+                    dist[adjNode] = adjWeight + distance; 
+                    q.push({dist[adjNode], adjNode});
                 }
-            }
-            
                 
-            
+            }
         }
         
-        for(int i = 0; i < dist.size(); i++) {
-            if(dist[i] == INT_MAX) {
-                dist[i] = -1; 
+        for(auto& val : dist) {
+            if(val == INT_MAX) {
+                val = -1; 
             }
         }
+        
         return dist;
+    }
+};
 ```
