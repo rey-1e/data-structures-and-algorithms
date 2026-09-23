75c89499caea6420285ba134041e145ec049df53
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-23 10:04:26 +0530

    feat(leetcode): solve Average Time of Process per Machine [C++]

--- LeetCode/Easy/Average-Time-of-Process-per-Machine.cpp
+++ LeetCode/Easy/Average-Time-of-Process-per-Machine.cpp
@@ -1,24 +1,28 @@
 # Average Time of Process per Machine
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/average-time-of-process-per-machine/submissions/2150416623/?envType=study-plan-v2&envId=top-sql-50
+- URL: https://leetcode.com/problems/average-time-of-process-per-machine/?envType=study-plan-v2&envId=top-sql-50
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
-- Runtime: 166 ms
+- Runtime: 115 ms
 - Memory: N/A
-- Solved At: 2026-09-23T04:21:28.861Z
+- Solved At: 2026-09-23T04:34:25.511Z
 
 ## Code
 ```cpp
 # Write your MySQL query statement below
-SELECT machine_id,
-    ROUND(SUM(
-        CASE 
-            WHEN activity_type = 'start' THEN -1 * timestamp
-            WHEN activity_type = 'end' THEN timestamp
-        END
-        ) / (COUNT(activity_type) / 2), 3) AS processing_time
-FROM Activity
+SELECT machine_id, ROUND(AVG(processing_time), 3) AS processing_time
+FROM (
+    SELECT machine_id,
+           SUM(
+            CASE
+                WHEN activity_type = 'start' THEN -1 * timestamp
+                WHEN activity_type = 'end' THEN timestamp
+           ) AS processing_time
+            END
+           FROM Activity
+           GROUP BY machine_id, process_id
+) AS temporary
 GROUP BY machine_id;
 ```
