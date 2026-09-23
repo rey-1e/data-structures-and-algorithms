dbdffa426295e512f7bd98aa4d2e181a42a7cb9b
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-23 12:11:54 +0530

    feat(leetcode): solve Not Boring Movies [C++]

--- LeetCode/Easy/Not-Boring-Movies.cpp
+++ LeetCode/Easy/Not-Boring-Movies.cpp
@@ -1,19 +1,19 @@
 # Not Boring Movies
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/not-boring-movies/submissions/2139352367/?utm_source=chatgpt.com
+- URL: https://leetcode.com/problems/not-boring-movies/submissions/2150566278/?envType=study-plan-v2&envId=top-sql-50
 - Difficulty: Easy
 - Language: C++
 - Status: Accepted
-- Runtime: 80 ms
+- Runtime: 76 ms
 - Memory: N/A
-- Solved At: 2026-09-12T09:41:55.286Z
+- Solved At: 2026-09-23T06:41:53.476Z
 
 ## Code
 ```cpp
 # Write your MySQL query statement below
 SELECT id, movie, description, rating
 FROM Cinema
-WHERE id % 2 != 0 AND description != "boring"
-ORDER BY rating DESC;
+WHERE id % 2 != 0 AND description != 'boring'
+ORDER by rating DESC;
 ```
