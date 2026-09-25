7da6e4ab28ff61d4e509600a43f11c2829d2faba
Raj Kanade <kanaderaj1216@gmail.com>
2026-09-25 18:26:04 +0530

    feat(leetcode): solve Last Person to Fit in the Bus [C++]

--- LeetCode/Medium/Last-Person-to-Fit-in-the-Bus.cpp
+++ LeetCode/Medium/Last-Person-to-Fit-in-the-Bus.cpp
@@ -1,28 +1,25 @@
 # Last Person to Fit in the Bus
 
 - Platform: LeetCode
-- URL: https://leetcode.com/problems/last-person-to-fit-in-the-bus/submissions/2150401281/
+- URL: https://leetcode.com/problems/last-person-to-fit-in-the-bus/submissions/2152983765/?envType=study-plan-v2&envId=top-sql-50
 - Difficulty: Medium
 - Language: C++
 - Status: Accepted
-- Runtime: 75 ms
+- Runtime: 63 ms
 - Memory: N/A
-- Solved At: 2026-09-23T04:06:16.934Z
+- Solved At: 2026-09-25T12:56:03.435Z
 
 ## Code
 ```cpp
 # Write your MySQL query statement below
 SELECT person_name
-FROM (
-    SELECT person_id,
-           person_name, 
-           weight, 
-           turn, 
-           SUM(weight) OVER(ORDER BY turn) AS rolling_total
-           
-           FROM Queue
-    ) AS tb
-WHERE rolling_total <= 1000
-ORDER BY turn DESC
+FROM 
+    (
+        SELECT *,
+            SUM(weight) OVER(ORDER BY turn ASC) AS total_sum
+        FROM Queue
+    ) AS tb1
+WHERE total_sum <= 1000
+ORDER BY total_sum DESC
 LIMIT 1;
 ```
